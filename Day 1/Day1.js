b73cff030b0c790bc58e02286dba479dabbb30ef
Aayush Gaur <aayush041003@gmail.com>
2023-01-01 20:48:31 +0530

    Day 1

---
+++ Day 1/Day1.js
@@ -0,0 +1,18 @@
+let firstName = 'Aayush'    
+let lastName = 'Gaur'     
+let country = 'India'      
+let city = 'Jaipur'         
+let age = 19               
+let isMarried = false
+
+const gravity = 9.81         
+const boilingPoint = 100   
+const PI = 3.14          
+
+let name = 'Aayush Gaur',     
+    job = 'Student',
+    live = 'India'
+
+console.log(firstName, lastName, country, city, age, isMarried);
+console.log(gravity, boilingPoint, PI); 
+console.log(name, job, live);
